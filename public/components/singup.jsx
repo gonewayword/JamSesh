774048e9aca862654c4b0a8a7fb0748705052754
Marina Cerame <marina.cerame@gmail.com>
2016-12-26 14:51:27 -0600

    outline of sign in

--- public/components/singup.jsx
+++ public/components/singup.jsx
@@ -3,8 +3,30 @@ class Signup extends React.Component {
     super(props);
     this.state = {
       username: '',
-      email: ''
+      email: '',
+      password: ''
     };
+
+    this.inputUsername = this.inputUsername.bind(this);
+    this.inputEmail = this.inputEmail.bind(this);
+    this.inputPassword = this.inputPassword.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit (event) {
+    // TODO: handle post request with this data
+    console.log('submitting data: ', this.state);
+    event.preventDefault();
+  }
+
+  inputUsername (event) {
+    this.setState({username: event.target.value});
+  }
+  inputEmail (event) {
+    this.setState({email: event.target.value});
+  }
+  inputPassword (event) {
+    this.setState({password: event.target.value});
   }
 
   render () {
@@ -12,9 +34,17 @@ class Signup extends React.Component {
       <div>
         <h3>Sign Up</h3>
 
-        <form>
-          <input type="text" value={this.state}></input>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.inputUsername} placeholder="Username"></input><br />
+          <input type="text" onChange={this.inputEmail} placeholder="Email"></input> <br />
+          <input type="password" onChange={this.inputPassword} placeholder="Password"></input> <br />
+          <button type="submit">Sign up</button>
         </form>
+
+        <div>
+          Already a member? <a href="signin.html">Sign in!</a>
+        </div>
+
       </div>
     );
   }
