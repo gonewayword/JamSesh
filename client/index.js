59130b233607f6c46bcc259e2f39bd2d2288add1
Marina Cerame <marina.cerame@gmail.com>
2016-12-26 11:39:50 -0600

    fix compile location

--- client/index.js
+++ client/index.js
@@ -1,2 +1,4 @@
 console.log('index.js loaded');
 ReactDOM.render(<App />, document.getElementById('app'));
+
+//babel . --out-dir client/compiled --presets=es2015,react --ignore=node_modules,compiled --source-maps inline
