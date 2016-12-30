f7bec34cb581327ae8376e17c4f6f47d93dade91
Marina Cerame <marina.cerame@gmail.com>
2016-12-30 08:16:02 -0600

    add bootstrap css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, browserHistory } from 'react-router';
+import { Link } from 'react-router';
 
 class Home extends React.Component {
   constructor(props) {
