c727568f0e31e628f64e4bcf9e7f5ef962105c9d
Marina Cerame <marina.cerame@gmail.com>
2016-12-30 15:44:17 -0600

    fix navbar buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,13 @@ const Navbar = () => (
     <div className="container">
       <ul className="nav nav-pills">
         <li>
-          <Link className="dark button" to="/"><span>Another Search</span></Link>
+          <Link className="dark button" to="/"><span>Home</span></Link>
         </li>
         <li>
           <Link className="dark button" to="sign-in"><span>Sign in</span></Link>
+        </li>
+        <li>
+          <Link className="dark button" to="sign-up"><span>Sign up</span></Link>
         </li>
         <li>
           <Link className="dark button" to="create-group"><span>Create Group</span></Link>
