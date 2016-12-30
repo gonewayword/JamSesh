507f0a1405e789bc2f4543c1a2a29a3c2b8878e7
Marina Cerame <marina.cerame@gmail.com>
2016-12-30 09:13:39 -0600

    style homepage, set table template

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './Navbar.jsx'
+import Navbar from './Navbar.jsx';
 
 class Home extends React.Component {
   constructor(props) {
@@ -10,7 +10,34 @@ class Home extends React.Component {
       <div className="container">
         <Navbar />
         <h2>MAIN</h2>
-        <div> this is where all our groups will be listed. eventually.</div>
+        <div className="col-md-8">
+          this is where all our groups will be listed. eventually.
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <td className="col-md-1">Inst.</td>
+                <td className="col-md-11">Details</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Test</td>
+                <td>Test</td>
+              </tr>
+              <tr>
+                <td>Test</td>
+                <td>Test</td>
+              </tr>
+              <tr>
+                <td>Test</td>
+                <td>Test</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="col-md-4">
+          this is where our filters will be. eventually.
+        </div>
       </div>
     );
   }
