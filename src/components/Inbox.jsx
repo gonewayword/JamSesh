50504a0da93c03728aeab8a97b34eed4d75cf362
Chris <christopherdakin1@gmail.com>
2017-01-02 14:38:03 -0600

    users can delete messages but they don't go away til refresh

--- src/components/Inbox.jsx
+++ src/components/Inbox.jsx
@@ -13,12 +13,18 @@ class Inbox extends React.Component {
     firebase.database().ref(`/messages/${user}`)
     .on('value', snapshot => {
       const snap = snapshot.val();
-      console.log(snap)
-      for (const prop in snap) {
-        this.state.messages.push(snap[prop]);
+      for(const key in snapshot.val()) {
+        console.log(key, 'key');
+        snap[key].exists = key;
+        this.state.messages.push(snap[key])
       }
+      // const snap = snapshot.val();
+      // console.log(snap, "snap");
+      // for (const prop in snap) {
+      //   this.state.messages.push([snap[prop], prop]);
+      // }
     });
-    console.log(this.state.messages);
+    console.log(this.state.messages, 'messages');
   }
   render() {
     const messages = this.state.messages;
--- src/components/InboxMessage.jsx
+++ src/components/InboxMessage.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
-
+import firebase from 'firebase';
 class InboxMessage extends React.Component {
   constructor(props) {
     super(props);
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    this.state = {
+      key: props,
+    }
   }
 
   handleDeleteClick() {
-    //
+    console.log('test');
+    console.log(this.state.key);
+    const user = firebase.auth().currentUser.displayName;
+    firebase.database().ref(`/messages/${user}/${this.state.key.item.exists}`).remove();
   }
   render() {
     return (
       <tr>
         <td>{this.props.item.fromUser}</td>
         <td>{this.props.item.message}</td>
-        <td><img src="https://www.comparethehighstreet.com/skins/default/styleImages/remove-location.png" /></td>
+        <td onClick={this.handleDeleteClick}><img src="https://www.comparethehighstreet.com/skins/default/styleImages/remove-location.png" /></td>
       </tr>
     );
   }
