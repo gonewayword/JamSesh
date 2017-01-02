3d4a736bbf12f2f364ac5b5c3a5e9ef2d8027e5f
Chris <christopherdakin1@gmail.com>
2017-01-02 12:20:18 -0600

    messages appear in inbox

--- src/components/Inbox.jsx
+++ src/components/Inbox.jsx
@@ -5,24 +5,36 @@ import firebase from 'firebase';
 class Inbox extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      messages: [],
+    };
+    const user = firebase.auth().currentUser.displayName;
+    console.log(user, 'test')
+    firebase.database().ref(`/messages/${user}`)
+    .on('value', snapshot => {
+      const snap = snapshot.val();
+      console.log(snap)
+      for (const prop in snap) {
+        this.state.messages.push(snap[prop]);
+      }
+    });
+    console.log(this.state.messages);
   }
   render() {
+    const messages = this.state.messages;
     return (
       <div className="container">
         <h2>Inbox</h2>
         <table className="table table-striped">
           <thead>
             <tr>
-              <td className="col-md-2">
-                Message From:
-              </td>
-              <td className="col-md-10">
-                Message:
-              </td>
+              <td className="col-md-2">Message From:</td>
+              <td className="col-md-8">Message:</td>
+              <td className="col-md-10">Delete</td>
             </tr>
           </thead>
           <tbody>
-            <InboxMessage />
+              {messages.map(el => <InboxMessage item={el} />)}
           </tbody>
         </table>
       </div>
--- src/components/InboxMessage.jsx
+++ src/components/InboxMessage.jsx
@@ -4,15 +4,16 @@ class InboxMessage extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  handleDeleteClick() {
+    //
+  }
   render() {
     return (
       <tr>
-        <td>
-          FromUser
-        </td>
-        <td>
-          Blah blah blah
-        </td>
+        <td>{this.props.item.fromUser}</td>
+        <td>{this.props.item.message}</td>
+        <td><img src="https://www.comparethehighstreet.com/skins/default/styleImages/remove-location.png" /></td>
       </tr>
     );
   }
